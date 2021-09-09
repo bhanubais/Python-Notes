494baea69e18df0f292ebf3cf6c2e190663b1320
Bhanu Bais <bhanubais@gmail.com>
2021-09-09 07:25:21 +0530

    Corrected: Prime Number Generator

---
+++ credit_card_scam_punish.py
@@ -0,0 +1,29 @@
+import requests
+import threading
+
+url = ''
+
+data = {
+	
+}
+
+def do_request():
+	while True:
+		response = requests.post(url, data = data).text
+		print(response)
+
+
+threads = []
+
+for i in range(50):
+	t = threading.Thread(target = do_request)
+	t.daemon = True
+	threads.append(t)
+
+for i in range(50):
+	threads[i].start()
+
+for i in range(50):
+	threads[i].join()
+
+
